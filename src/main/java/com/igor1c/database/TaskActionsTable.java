73f28ab423e4ad2cf6c281ffc2763160d3f879a0
Igor <voittamaton1@gmail.com>
2019-12-22 15:04:02 +0700

    Work

---
+++ src/main/java/com/igor1c/database/TaskActionsTable.java
@@ -0,0 +1,37 @@
+package com.igor1c.database;
+
+public class TaskActionsTable extends TableController {
+
+    public TaskActionsTable() {
+        super("taskActions");
+    }
+
+    public void createTable() {
+
+        String query =  "CREATE TABLE taskActions(\n" +
+                        "   id BIGINT AUTO_INCREMENT PRIMARY KEY,\n" +
+                        "   name VARCHAR(255) NOT NULL,\n" +
+                        "   actionType BIGINT,\n" +
+                        "   userTask BIGINT\n" +
+                        ");";
+
+        executeQuery(query);
+
+    }
+
+    public void createForeignKeys() {
+
+        String query =  "ALTER TABLE taskActions\n" +
+                        "   ADD FOREIGN KEY (actionType)\n" +
+                        "   REFERENCES actionTypes(id);\n" +
+                        "ALTER TABLE taskActions\n" +
+                        "   ADD FOREIGN KEY (userTask)\n" +
+                        "   REFERENCES userTasks(id);";
+
+        executeQuery(query);
+
+    }
+
+    public void createExtraConstraints() {}
+
+}
--- src/main/java/com/igor1c/taskmanager/Application.java
+++ src/main/java/com/igor1c/taskmanager/Application.java
@@ -1,12 +1,5 @@
 package com.igor1c.taskmanager;
 
-import com.igor1c.database.ActionTypeParamsTable;
-import com.igor1c.database.ActionTypesTable;
-import com.igor1c.database.TableOperations;
-import com.igor1c.database.UserTasksTable;
-
-import java.util.ArrayList;
-
 public class Application {
 
     public static void main(String ... args) {
