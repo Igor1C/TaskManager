8adf0e799f2227e56034cf279c167a33ca369cab
Igor <voittamaton1@gmail.com>
2019-12-24 00:09:42 +0700

    Work

---
+++ src/main/java/com/igor1c/controllers/UserTaskController.java
@@ -0,0 +1,4 @@
+package com.igor1c.controllers;
+
+public class UserTaskController {
+}
--- src/main/java/com/igor1c/taskmanager/Application.java
+++ src/main/java/com/igor1c/taskmanager/Application.java
@@ -1,10 +1,39 @@
 package com.igor1c.taskmanager;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.HashMap;
+
+@SpringBootApplication
 public class Application {
+
+    final static String DEFAULT_PORT = "32380";
 
     public static void main(String ... args) {
 
-        DBHelper.createDatabase();
+        HashMap<String, Object> props = new HashMap<>();
+        setPort(args, props);
+
+        new SpringApplicationBuilder()
+                .sources(Application.class)
+                .properties(props)
+                .run(args);
+
+        //DBHelper.createDatabase();
+
+    }
+
+    private static void setPort(String args[], HashMap<String, Object> props) {
+
+        String port;
+
+        if (args.length > 0)
+            port = args[0];
+        else
+            port = DEFAULT_PORT;
+
+        props.put("server.port", port);
 
     }
 
