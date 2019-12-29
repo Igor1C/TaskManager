f10c294c9e13df56e46c3090afdbeccaefe1d7bb
Igor <voittamaton1@gmail.com>
2019-12-29 19:07:50 +0700

    Work

--- src/main/java/com/igor1c/taskmanager/controllers/MainController.java
+++ src/main/java/com/igor1c/taskmanager/controllers/MainController.java
@@ -3,18 +3,21 @@ package com.igor1c.taskmanager.controllers;
 import com.igor1c.taskmanager.database.ActionTypesTable;
 import com.igor1c.taskmanager.entities.BaseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 
 @Controller
-public class UserTaskController {
+public class MainController {
 
     @GetMapping("")
-    public String main() {
+    public String main(Model model) {
 
         ActionTypesTable actionTypesTable = new ActionTypesTable();
         ArrayList<BaseEntity> entityArrayList = actionTypesTable.select();
+
+        model.addAttribute("actionTypes", entityArrayList);
 
         return "main.html";
 
--- src/main/resources/templates/main2.ftl
+++
@@ -1,9 +0,0 @@
-<#import "parts/common.ftl" as c>
-
-<@c.page>
-    <div class="btn-group">
-        <button type="button" class="btn btn-success">This is a success button</button>
-        <button type="button" class="btn btn-warning">This is a warning button</button>
-        <button type="button" class="btn btn-danger">This is a danger button</button>
-    </div>
-</@c.page>
